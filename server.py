68e246d053164bd9e2eb57e5e67b798d8e7cb7f2
Nathan Gaberel <nathang@theodo.co.uk>
2016-06-11 14:49:53 +0100

    Enable CORS

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
+from flask.ext.cors import CORS
 from tint import from_url, hex
+
 application = Flask(__name__)
+cors = CORS(application)
 
 @application.route('/<path:url>')
 def tint(url):
