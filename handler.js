9f1bbdaac21f39eecdaceed9e4a06d57b03e4f26
Nathan Gaberel <nathang@theodo.co.uk>
2017-07-31 23:57:48 +0100

    Use sls-compose.

--- handler.js
+++ handler.js
@@ -1,23 +1,25 @@
+const { compose, middlewares, res } = require('sls-compose')
 const colorPalette = require('./lib/palette')
 const download = require('./lib/download')
 
 const coloursCount = 3
 
 module.exports.usage = (event, context, cb) => {
-  cb(null, {
-    headers: {
-      'Content-Type': 'text/html'
-    },
-    body: require('./usage')
-  })
+  cb(
+    null,
+    res.html(require('./usage'))
+  )
 }
 
-module.exports.tint = (event, context, cb) => {
-  const { url } = JSON.parse(event.body)
+module.exports.tint = compose(
+  middlewares.parseBody,
+  (event, context, cb) => {
+    download(event.body.url)
+    .then(filePath => colorPalette(filePath, coloursCount))
+    .then(colours => cb(
+      null,
+      res.json({ colours })
+    ))
+  }
 
-  download(url)
-  .then(filePath => colorPalette(filePath, coloursCount))
-  .then(colours => cb(null, {
-    body: JSON.stringify({ colours })
-  }))
-}
+)
